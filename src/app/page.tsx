f1777736aa383a47f32eed37c04d101f4042922b
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-17 15:33:27 +0100

    add page view counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { kv } from "@vercel/kv"
+
 
 export default async function Home() {
   const res = await fetch("https://api.github.com/repos/adesewa293/view-counter", { next: {revalidate:5}});
   const data = await res.json();
+
+  const pageViews = await kv.incr("views");
+  const myStyles = {
+    p:{
+fontSize: pageViews + "rem",
+    },
+  };
   return (
     <main>
-      {data.stargazers_count}
+      <p style={myStyles.p}>{pageViews}</p><span>⭐️{data.stargazers_count}</span>
     </main>
   )
 }
